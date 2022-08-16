24dd55e57d6e510f041f3bee5068f10de6427f9c
RachaelMuriithi <rachael.mureithi@student.moringaschool.com>
2022-08-16 08:21:09 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+let currentUser = 'James';
+var welcomeMessage = "Welcome to Flatbook, " + 'James!'; 
+var excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + 'JAMES!';
+var shortGreeting = "Welcome, " +  'J!';
